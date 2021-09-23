91754ff05ccfe3f61e7b964f2f7c13a53ffbf3be
daotzen98 <91252675+daotzen98@users.noreply.github.com>
2021-09-23 22:36:23 +0200

    Har lavet en method, "roll", så der ikke er gentagelser i koden og man lettere kan tilføje nye spillere.

--- src/Terninger.java
+++ src/Terninger.java
@@ -2,39 +2,35 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Terninger {
-        public static void main(String[] args) {
-            Random rn = new Random();
 
-            int low = 1;
-            int high = 6;
-
-            int counter1 = 0;
-            int counter2 = 0;
-
-            while(counter1 < 40 & counter2 < 40) {
-                int result = rn.nextInt(high-low) + low;
-                int result2 = rn.nextInt(high-low) + low;
-                System.out.print("Spiller 1, slå terningen");
-                Scanner sc = new Scanner(System.in);
-                String roll = sc.nextLine();
-                System.out.println("Du slog " + result + " og " + result2);
-
-                counter1 += result + result2;
-                System.out.println("Du har " +counter1 + " point!\n");
-
-                int result3 = rn.nextInt(high-low) + low;
-                int result4 = rn.nextInt(high-low) + low;
-                System.out.print("Spiller 2, slå terningen");
-                Scanner sc1 = new Scanner(System.in);
-                String roll1 = sc1.nextLine();
-                System.out.println("Du slog " + result3 + " og " + result4);
-
-                counter2 += result3 + result4;
-                System.out.println("Du har " +counter2 + " point!\n");
-            }
-            if(counter1 > counter2) {
-                System.out.println("Spiller 1 har vundet!!!");
-            }
-            else System.out.println("Spiller 2 har vundet!!!");
-        }
+    public static int roll(int player) {
+        Random rn = new Random();
+        int low = 1;
+        int high = 6;
+        int result = rn.nextInt(high - low) + low;
+        int result2 = rn.nextInt(high - low) + low;
+        System.out.print("Spiller " + player + ", slå terningen");
+        Scanner sc = new Scanner(System.in);
+        String r = sc.nextLine();
+        System.out.println("Du slog " + result + " og " + result2);
+        return (result + result2);
     }
+
+    public static void main(String[] args) {
+
+        int counter1 = 0;
+        int counter2 = 0;
+
+        while (counter1 < 40 & counter2 < 40) {
+            counter1 += roll(1);
+            System.out.println("Du har " +counter1 + " point!\n");
+
+            counter2 += roll(2);
+            System.out.println("Du har " +counter2 + " point!\n");
+        }
+        if(counter1 > counter2) {
+            System.out.println("Spiller 1 har vundet!!!");
+        }
+        else System.out.println("Spiller 2 har vundet!!!");
+    }
+}
