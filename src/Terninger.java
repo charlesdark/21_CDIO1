ab4a71033c9b66a289d634a3bc0ad66c18d1c7ea
daotzen98 <91252675+daotzen98@users.noreply.github.com>
2021-09-23 22:49:52 +0200

    Tilføjede navne og tilføjede en if-break, i midten af loopet

--- src/Terninger.java
+++ src/Terninger.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 
 public class Terninger {
 
-    public static int roll(int player) {
+    public static int roll(String player) {
         Random rn = new Random();
         int low = 1;
         int high = 6;
         int result = rn.nextInt(high - low) + low;
         int result2 = rn.nextInt(high - low) + low;
-        System.out.print("Spiller " + player + ", slå terningen");
+        System.out.print(player + ", slå terningen");
         Scanner sc = new Scanner(System.in);
         String r = sc.nextLine();
         System.out.println("Du slog " + result + " og " + result2);
@@ -17,20 +17,28 @@ public class Terninger {
     }
 
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Navn på Spiller 1: ");
+        String playerName1 = scan.nextLine();
+        System.out.println("Navn på Spiller 2: ");
+        String playerName2 = scan.nextLine();
 
         int counter1 = 0;
         int counter2 = 0;
 
         while (counter1 < 40 & counter2 < 40) {
-            counter1 += roll(1);
+            counter1 += roll(playerName1);
             System.out.println("Du har " +counter1 + " point!\n");
 
-            counter2 += roll(2);
+            if(counter1>40)
+                break;
+
+            counter2 += roll(playerName2);
             System.out.println("Du har " +counter2 + " point!\n");
         }
         if(counter1 > counter2) {
-            System.out.println("Spiller 1 har vundet!!!");
+            System.out.println(playerName1 + " har vundet!!!");
         }
-        else System.out.println("Spiller 2 har vundet!!!");
+        else System.out.println(playerName2 +" har vundet!!!");
     }
 }
