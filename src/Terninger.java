de838643eb0298627083cea0dce04533e5c27c98
daotzen98 <91252675+daotzen98@users.noreply.github.com>
2021-09-27 13:26:41 +0200

    Tilføjet kommentarer

--- src/Terninger.java
+++ src/Terninger.java
@@ -4,19 +4,23 @@ import java.util.Scanner;
 public class Terninger {
 
     public static void main(String[] args) {
+        //Opretter Scanner class
         Scanner scan = new Scanner(System.in);
         System.out.println("Navn på Spiller 1: ");
         String playerName1 = scan.nextLine();
         System.out.println("Navn på Spiller 2: ");
         String playerName2 = scan.nextLine();
 
+        //Opretter en counter pr. spiller
         int counter1 = 0;
         int counter2 = 0;
 
+        //Laver et while loop som breaker når én af counterne når >40
         while (counter1 < 40 & counter2 < 40) {
-            counter1 += Roll.roll(playerName1);
+            counter1 += Roll.roll(playerName1); //Counter1 stiger med sidste roll
             System.out.println("Du har " +counter1 + " point!\n");
 
+            //Hvis counter1 er over 40, skal loopen breakes inden den når spiller 2s tur
             if(counter1>40)
                 break;
 
