d1ba3352b6475f05fb66c0a345019183913c97d6
SimonP2602 <91070384+SimonP2602@users.noreply.github.com>
2021-09-28 12:30:30 +0200

    Tilføjet feature hvor at hvis man slår to 1'ere mister man sine point

--- src/Terninger.java
+++ src/Terninger.java
@@ -18,12 +18,20 @@ public class Terninger {
 
         //Laver et while loop som breaker når én af counterne når >40
         while (counter1 < 40 & counter2 < 40) {
-            counter1 += Roll.roll(playerName1); //Counter1 stiger med sidste roll
-            System.out.println("Du har " +counter1 + " point!\n");
+            int result = Roll.roll(playerName1);
+            if(result == 2){
+                counter1 = 0;
+            }
+            else counter1 += result; //Counter1 stiger med sidste roll
+            System.out.println("Du har " + counter1 + " point!\n");
 
+            int result2 = Roll.roll(playerName2);
+            if (result2 == 2){
+                counter2 == 0;
+            }
+            else counter2 += result2;
+            System.out.println("Du har " + counter2 + " point!\n");
 
-            counter2 += Roll.roll(playerName2);
-            System.out.println("Du har " +counter2 + " point!\n");
         }
         if(counter1 > counter2) {
             System.out.println(playerName1 + " har vundet!!!");
