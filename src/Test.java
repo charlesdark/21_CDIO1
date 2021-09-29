c372d577f454161dda054e183a3a73fdd916c639
AhmedNazo <91070949+AhmedNazo@users.noreply.github.com>
2021-09-29 13:31:59 +0200

    test af terningkast. v1

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,29 @@
+import java.util.Random;
 
+    //dicetest for at vise antal forekomster af mulige værdier af summen
+    class test {
 
-public class Test {
+    public static void main(String[] args) {
+        int sum, dice1, dice2, actual = 0;
+        int[] Total = new int[13];
+        int[] ways = {0, 0, 1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1 };
 
+        for (int i = 0; i < 1000; i++) {
+            sum = 0;
+            dice1 = new Random().nextInt(6) + 1;
+            dice2 = new Random().nextInt(6) + 1;
+            sum = dice1 + dice2;
+            Total[sum]++;
+        }
+
+        for (int i = 0; i < 13; i++) actual += Total[i];
+
+        //sout bliver mere overskueligt at aflæse
+        System.out.printf("%-4s %s %-11s %s%n", "Sum", "Total", "Expected",
+                "Actual");
+        for (int i = 2; i < 13; i++)
+            System.out.printf("%-4d %-5d %-10.5f %2.5f%n", i, Total[i],
+                    100.0 * ways[i] / 36, 100.0 * (double) Total[i] / actual);
     }
-
+}
 
