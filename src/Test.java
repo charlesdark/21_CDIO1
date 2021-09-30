472de77a099f608e492e067dd1ca85ae14617d56
AhmedNazo <91070949+AhmedNazo@users.noreply.github.com>
2021-09-30 08:44:28 +0200

    indsat counter for to ens

--- src/Test.java
+++ src/Test.java
@@ -1,3 +1,4 @@
+import java.io.PrintStream;
 import java.util.Random;
 
     //dicetest for at vise antal forekomster af mulige værdier af summen
@@ -6,12 +7,17 @@ import java.util.Random;
     public static void main(String[] args) {
         int sum, dice1, dice2, actual = 0;
         int[] Total = new int[13];
-        int[] ways = {0, 0, 1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1 };
-
+        int[] ways = {0, 0, 1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1};
+        int cnt = 0;
         for (int i = 0; i < 1000; i++) {
             sum = 0;
             dice1 = new Random().nextInt(6) + 1;
             dice2 = new Random().nextInt(6) + 1;
+            // counter for to ens
+            if (dice1 == dice2) {
+                cnt++;
+            }
+
             sum = dice1 + dice2;
             Total[sum]++;
         }
@@ -21,9 +27,14 @@ import java.util.Random;
         //sout bliver mere overskueligt at aflæse
         System.out.printf("%-4s %s %-11s %s%n", "Sum", "Total", "Expected",
                 "Actual");
-        for (int i = 2; i < 13; i++)
+        for (int i = 2; i < 13; i++) {
             System.out.printf("%-4d %-5d %-10.5f %2.5f%n", i, Total[i],
                     100.0 * ways[i] / 36, 100.0 * (double) Total[i] / actual);
+    }
+        {
+            System.out.printf("to ens = " + cnt);
+
+        }
     }
 }
 
