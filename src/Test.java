cb15e0905ac9a3b69baa93c6bf3e0de4771c938c
daotzen98 <91252675+daotzen98@users.noreply.github.com>
2021-09-30 12:28:08 +0200

    Test er lavet simplere

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,28 @@
-
-
 public class Test {
+    public static void main(String[] args) {
 
+        Dice dice = new Dice();
+
+        int sum, actual = 0;
+        int[] Total = new int[14];
+
+        for (int i = 0; i < 1000; i++) {
+            dice.roll1();
+            dice.roll2();
+            sum = Dice.getDots1() + Dice.getDots2();
+            if (Dice.getDots1() == Dice.getDots2()) {
+                Total[13]++;
+                Total[sum]++;
+            }
+            else Total[sum]++;
+        }
+
+        for (int i = 0; i < 14; i++) actual += Total[i];
+
+        for (int i = 2; i < 14; i++)
+            System.out.println(i + "    " + Total[i] + "    " + 100.0 * (double) Total[i] / actual +" %");
     }
+}
+
 
 
