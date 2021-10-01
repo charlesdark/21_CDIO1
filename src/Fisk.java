425c5eeeab55b8771a31bb500d211df2b872b2a4
PeterM0ller <s215707@dtu.dk>
2021-10-01 14:15:48 +0200

    Lavet ny branch og har prøvet at lave så man vinder med 2 ens

--- src/Fisk.java
+++ src/Fisk.java
@@ -21,12 +21,12 @@ public class Fisk {
                 dice.roll2();
                 System.out.println(playerName1 + " Tryk Enter for at slå med terningen");
                 scan.nextLine();
-                if(Dice.getDots1() + Dice.getDots2() == 2) {
-                    counter1 = 0;}
-                else counter1 += Dice.getDots1() + Dice.getDots2();
+                if (Dice.getDots1() + Dice.getDots2() == 2) {
+                    counter1 = 0;
+                } else counter1 += Dice.getDots1() + Dice.getDots2();
                 System.out.println(playerName1 + ", du slog " + Dice.getDots1() + " og " + Dice.getDots2() + "\n Du har " + counter1 + " point!\n");
 
-                if(Dice.getDots1()==Dice.getDots2()){
+                if (Dice.getDots1() == Dice.getDots2()) {
                     System.out.println(playerName1 + " har fået en ekstra tur");
                 }
             }
@@ -38,24 +38,95 @@ public class Fisk {
                 dice.roll2();
                 System.out.println(playerName2 + " Tryk Enter for at slå med terningen");
                 scan.nextLine();
-                if(Dice.getDots1() + Dice.getDots2() == 2) {
-                    counter2 = 0;}
-                else counter2 += Dice.getDots1() + Dice.getDots2();
+                if (Dice.getDots1() + Dice.getDots2() == 2) {
+                    counter2 = 0;
+                } else counter2 += Dice.getDots1() + Dice.getDots2();
                 System.out.println(playerName2 + ", du slog " + Dice.getDots1() + " og " + Dice.getDots2() + "\n Du har " + counter2 + " point!\n");
 
-                if(Dice.getDots1()==Dice.getDots2()) {
+                if (Dice.getDots1() == Dice.getDots2()) {
                     System.out.println(playerName2 + " har fået en ekstra tur");
                 }
 
             }
             while (Dice.getDots1() == Dice.getDots2());
         }
-        if (counter1 > counter2){
-            System.out.println(playerName1 + " vandt!!!");
+
+        if (counter1 >= 40) {
+            while (Dice.getDots1() != Dice.getDots2()) {
+                dice.roll1();
+                dice.roll2();
+                System.out.println(playerName1 + " Tryk Enter for at slå med terningen");
+                scan.nextLine();
+                System.out.println(playerName1 + ", du slog " + Dice.getDots1() + " og " + Dice.getDots2() + "\n Du har " + counter1 + " point!\n");
+
+                dice.roll1();
+                dice.roll2();
+                System.out.println(playerName2 + " Tryk Enter for at slå med terningen");
+                scan.nextLine();
+                counter2 += Dice.getDots1() + Dice.getDots2();
+                System.out.println(playerName2 + ", du slog " + Dice.getDots1() + " og " + Dice.getDots2() + "\n Du har " + counter2 + " point!\n");
+                if (counter2 >= 40) {
+                    break;
+
+                }
+
+            }
+            if (counter2<40){
+                System.out.println(playerName1+ " har vundet!!!");
+                System.exit(0);}
+
         }
-        if (counter1 == counter2){
-            System.out.println("Uafgjort");
+        if (counter2 >= 40) {
+            while (Dice.getDots1() != Dice.getDots2()) {
+                dice.roll1();
+                dice.roll2();
+                System.out.println(playerName1 + " Tryk Enter for at slå med terningen");
+                scan.nextLine();
+                counter1 += Dice.getDots1() + Dice.getDots2();
+                System.out.println(playerName1 + ", du slog " + Dice.getDots1() + " og " + Dice.getDots2() + "\n Du har " + counter1 + " point!\n");
+
+                dice.roll1();
+                dice.roll2();
+                System.out.println(playerName2 + " Tryk Enter for at slå med terningen");
+                scan.nextLine();
+                System.out.println(playerName2 + ", du slog " + Dice.getDots1() + " og " + Dice.getDots2() + "\n Du har " + counter2 + " point!\n");
+
+                if (counter1 >= 40) {
+                    break;
+
+                }
+            } if (counter1<40){
+                System.out.println(playerName2+ " har vundet!!!");
+                System.exit(0);}
+
         }
-        else System.out.println(playerName2 + " vandt!!!");
+        int counter3=0;
+        int counter4=0;
+        while (Dice.getDots1() != Dice.getDots2()){
+
+            dice.roll1();
+            dice.roll2();
+            System.out.println(playerName1 + " Tryk Enter for at slå med terningen");
+            scan.nextLine();
+            counter3++;
+            System.out.println(playerName1 + ", du slog " + Dice.getDots1() + " og " + Dice.getDots2() + "\n Du har " + counter1 + " point!\n");
+            if(Dice.getDots1()==Dice.getDots2()){
+                System.out.println(playerName1+ " har vundet!!!");
+                System.exit(0);
+            }
+
+            dice.roll1();
+            dice.roll2();
+            System.out.println(playerName2 + " Tryk Enter for at slå med terningen");
+            scan.nextLine();
+            counter4++;
+            System.out.println(playerName2 + ", du slog " + Dice.getDots1() + " og " + Dice.getDots2() + "\n Du har " + counter2 + " point!\n");
+        }
+        if (counter3>counter4){
+            System.out.println(playerName1+ " har vundet!!!");
+        }
+        if (counter4 == counter3){
+            System.out.println(playerName2+ " har vundet!!!");
+        }
     }
 }
